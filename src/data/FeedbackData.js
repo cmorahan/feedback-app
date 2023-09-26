dacf9fc14fa09a831d60af216dfa38da8318d734
cmorahan <caragh.morahan@analog.com>
2023-09-26 14:03:55 +0100

    - created feedbackApp

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "bruh moment.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Cringe cringe cringe cringe",
+  },
+];
+
+export default FeedbackData;
